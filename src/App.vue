d01327916ce83844c2726106d0af96e70c31ae23
mohamed <mohamed.mohamed.mhiri@gmail.com>
2017-10-13 00:11:55 +0100

    moom table component in progress #2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="app">
-    
+    <moom-table></moom-table>
   </div>
 </template>
 
 <script>
+import MoomTable from './components/moom-table.vue'
 export default {
   name: 'app',
+  components: {MoomTable},
   data () {
     return {
     }
--- src/components/moom-table.vue
+++ src/components/moom-table.vue
@@ -1,70 +1,27 @@
 <template>
     <div class="ui container">
-  <table class="ui celled table">
-    <thead>
-      <th>Employee</th>
-      <th>Correct Guesses</th>
-    </thead>
-    <tbody>
-      <tr>
-        <td>
-          <h4 class="ui image header">
-            <img src="assets/images/wireframe/square-image.png" class="ui mini rounded image">
-            <div class="content">
-              Lena
-              <div class="sub header">Human Resources
-            </div>
-          </div>
-        </td>
-        <td>
-          22
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h4 class="ui image header">
-            <img src="assets/images/wireframe/square-image.png" class="ui mini rounded image">
-            <div class="content">
-              Matthew
-              <div class="sub header">Fabric Design
-            </div>
-          </div>
-        </td>
-        <td>
-          15
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h4 class="ui image header">
-            <img src="assets/images/wireframe/square-image.png" class="ui mini rounded image">
-            <div class="content">
-              Lindsay
-              <div class="sub header">Entertainment
-            </div>
-          </div>
-        </td>
-        <td>
-          12
-        </td>
-      </tr>
-      <tr>
-        <td>
-          <h4 class="ui image header">
-            <img src="assets/images/wireframe/square-image.png" class="ui mini rounded image">
-            <div class="content">
-              Mark
-              <div class="sub header">Executive
-            </div>
-          </div>
-        </td>
-        <td>
-          11
-        </td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+      <table class="ui celled table">
+        <thead>
+          <th>Employee</th>
+          <th>Correct Guesses</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <h4 class="ui image header"></h4>
+                <div class="content">
+                  Mark
+                  <div class="sub header">Executive
+                </div>
+              </div>
+            </td>
+            <td>
+              11
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
 </template>
 
 <script>
